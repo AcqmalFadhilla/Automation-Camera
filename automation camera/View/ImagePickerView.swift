25e4d88f9e139627a7f72e79f11026dd7b3a5c4f
acqmalfadhilla <acqmalfadhillalatief@gmail.com>
2025-05-01 01:17:52 +0700

    feat: add ImagePicker and CameraScreenView
    Created ImagePicker as a swiftUI bridge to UIKit

---
+++ automation camera/View/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  automation camera
+//
+//  Created by acqmal on 5/1/25.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
