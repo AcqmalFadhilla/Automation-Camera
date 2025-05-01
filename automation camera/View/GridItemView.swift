32ea4b7eb841db3912aeecae44e05db1dd093fb1
acqmalfadhilla <acqmalfadhillalatief@gmail.com>
2025-05-01 12:16:19 +0700

    feat: add GalleryView and GridItemView for image grid UI

---
+++ automation camera/View/GridItemView.swift
@@ -0,0 +1,18 @@
+//
+//  GridItemView.swift
+//  automation camera
+//
+//  Created by acqmal on 5/1/25.
+//
+
+import SwiftUI
+
+struct GridItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridItemView()
+}
